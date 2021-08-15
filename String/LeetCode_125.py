ff0dc7aef4cd4439047f37dd2e8487f3467d9ee8
sua-kim <sua1021606@gmail.com>
2021-08-15 23:58:41 +0900

    feat: 8/15 LeetCode_125

--- String/LeetCode_125.py
+++ String/LeetCode_125.py
@@ -2,6 +2,8 @@
 LeetCode_125 - 유효한 팰린드롬
 '''
 
+# 나의 풀이
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         string_list = []
@@ -13,3 +15,27 @@ class Solution:
             return True
         else:
             return False
+
+
+# 풀이 1: 리스트로 변환
+class Solution1:
+    def isPalindrome(self, s: str) -> bool:
+        strs = []
+        for char in str:
+            if char.isalnum():
+                strs.append(char.lower())
+        # 팰린드롬 여부 판별
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
+        return True
+
+# 풀이 2: 데크 자료형을 이용한 최적화 (10장 이후 시도)
+
+# 풀이 3: 슬라이싱 사용
+import re
+class Solution3:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub('[^a-z0-9]', '', s)
+        return s == s[::-1]
