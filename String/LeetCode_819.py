59fba60ba80d9eb0f9aa153c5984596575cdcb01
sua-kim <sua1021606@gmail.com>
2021-08-16 23:23:42 +0900

    feat: 8/16 LeetCode_819

--- String/LeetCode_819.py
+++ String/LeetCode_819.py
@@ -18,3 +18,10 @@ class Solution:
                 common_words = str
 
         return common_words
+
+# 풀이 1
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
+        counts = collections.Counter(words)
+        return counts.most_common(1)[0][0]
