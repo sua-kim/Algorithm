6d908ecbd71f2ec5f01ede593f8d3baa4c318a66
sua-kim <sua1021606@gmail.com>
2021-09-26 20:43:58 +0900

    feat: 9/26 BOJ_15655

---
+++ Backtracking/BOJ_15655.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+N_list = list(map(int, input().split()))
+N_list.sort()
+a = [0]*M
+def recursion(index, start, N, M):
+    if index == M:
+        print(' '.join(map(str, a)))
+        return
+    for i in range(start, N):
+        a[index] = N_list[i]
+        recursion(index+1, i+1, N, M)
+        a[index] = 0
+
+recursion(0, 0, N, M)
