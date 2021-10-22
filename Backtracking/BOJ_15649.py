bc4f156f8cd893779338f06a81e034e3312cb8e5
sua-kim <sua1021606@gmail.com>
2021-10-22 22:08:50 +0900

    feat: 10/22

--- Backtracking/BOJ_15649.py
+++ Backtracking/BOJ_15649.py
@@ -20,3 +20,14 @@ def recursion(index, N, M):
             c[i] = False
 
 recursion(0, N, M)
+
+
+'''
+from itertools import permutations
+
+n, m = map(int, input().split())
+n_list = [i for i in range(1, n+1)]
+p_list = list(permutations(n_list, m))
+for p in p_list:
+    print(' '.join(map(str, p)))
+'''
